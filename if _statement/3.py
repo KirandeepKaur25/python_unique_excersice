f0e4d718fb67ebfd96f03b2a6037d908586a01a6
KirandeepKaur01 <kaurkirandeep8808@gmail.com>
2025-08-17 18:19:23 -0400

    additional operator used to construct more complicated conditions

---
+++ if _statement/3.py
@@ -0,0 +1,12 @@
+# conditional operator
+''' if x>3: if x is greater than 3
+if x>= 3: if x is greater than or equal to 3
+if x == 3 : if x is 3
+if x != 3 : if x is not 3 '''
+if grade>=80 and grade<90:
+    print('your grade is a B.')
+
+if score>1000 or time>20:
+    print('Game over')
+if not (score>1000 or time>20):
+    print('Game continues.')
