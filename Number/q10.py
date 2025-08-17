5d103140af7bcd78f5c08caf9a3e00d01c798b63
KirandeepKaur01 <kaurkirandeep8808@gmail.com>
2025-08-16 22:31:21 -0400

    find the last two digit of 2 raised to that power.

---
+++ Number/q10.py
@@ -0,0 +1,8 @@
+'''One way to find out the last digit of a number is to mod the number by 10. Write a
+program that asks the user to enter a power. Then find the last digit of 2 raised to that
+power'''
+power = int(input("Enter the power:"))
+result = 2**power
+last_digit = result % 10
+print(f"2 raised to the power of {power} is {result}")
+print(f"The last digit is : {last_digit}")
