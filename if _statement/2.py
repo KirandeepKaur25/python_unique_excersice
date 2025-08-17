ad5cb5e3fc638293b96d83fd683927394483fa35
KirandeepKaur01 <kaurkirandeep8808@gmail.com>
2025-08-16 23:58:44 -0400

    if the player guess wrong

--- if _statement/2.py
+++ if _statement/2.py
@@ -1,5 +1,7 @@
+from random import randint
+num = randint(1,10)
+guess = eval(input("enter your guess:"))
 if guess==num:
     print('You got it!')
 else:
     print('Sorry. The number is ', num)
-    
